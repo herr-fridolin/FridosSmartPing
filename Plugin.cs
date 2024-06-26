bf09de620f8c2e29a882a73cf3f433ab5717dd05
herrfridolin <basti-moser@web.de>
2024-06-26 16:04:39 +0200

    changed message and determining relevant zone

--- Plugin.cs
+++ Plugin.cs
@@ -4,10 +4,12 @@ using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using LevelGeneration;
 using Localization;
+using Player;
+using SNetwork;
 
 namespace FridosSmartPing
 {
-    [BepInPlugin("com.fridos.smartping", "FridosSmartPing", "1.2.2")]
+    [BepInPlugin("com.fridos.smartping", "FridosSmartPing", "1.2.5")]
     public class SmartPing : BasePlugin
     {
         public static List<ItemInLevel> itemList = new();
@@ -29,7 +31,8 @@ namespace FridosSmartPing
                         {
                             if (name.Contains("PACK") || name.StartsWith("TOOL_REFILL") ||
                                 name.StartsWith("KEY_") || name.StartsWith("PID_") || name.StartsWith("BULKHEAD_KEY_") ||
-                                name.StartsWith("DATA_CUBE_") || name.StartsWith("CELL_") || name.StartsWith("FOG_TURBINE"))
+                                name.StartsWith("DATA_CUBE_") || name.StartsWith("CELL_") || name.StartsWith("FOG_TURBINE") || name.StartsWith("GLP_") ||
+                                    name.StartsWith("PD_") || name.StartsWith("OSIP_") || name.StartsWith("HDD_") || name.StartsWith("PLANT_"))
                             {
                                 itemList.Add(item);
                             }
@@ -60,31 +63,28 @@ namespace FridosSmartPing
             },TERM_CommandRule.Normal);
         }
     }
-    /*[HarmonyPatch(typeof(ItemInLevel), nameof(ItemInLevel.OnPickedUp))]
+    [HarmonyPatch(typeof(ItemInLevel), nameof(ItemInLevel.OnPickedUp))]
     public class RemoveNavMarkerPatch
     {
         static void Postfix(ItemInLevel __instance, PlayerAgent player, InventorySlot slot, AmmoType ammoType)
         {
-            if(SNet.IsMaster)
+            LG_GenericTerminalItem component = __instance.GetComponentInChildren<LG_GenericTerminalItem>();
+            if (component != null)
             {
-                LG_GenericTerminalItem component = __instance.GetComponentInChildren<LG_GenericTerminalItem>();
-                if (component != null)
+                var name = component.TerminalItemKey;
+                if (name != null)
                 {
-                    var name = component.TerminalItemKey;
-                    if (name != null)
+                    foreach (NavMarker nM in GuiManager.NavMarkerLayer.m_markersActive)
                     {
-                        foreach (NavMarker nM in GuiManager.NavMarkerLayer.m_markersActive)
+                        if (nM != null)
                         {
-                            if (nM != null)
+                            if (nM.m_title != null && nM.m_title.text != null)
                             {
-                                if (nM.m_title != null && nM.m_title.text != null)
+                                if (nM.m_title.text.Contains(component.TerminalItemKey))
                                 {
-                                    if (nM.m_title.text.Contains(component.TerminalItemKey))
-                                    {
-                                        nM.SetVisible(false);
-                                        nM.m_currentState = NavMarkerState.Inactive;
-                                        break;
-                                    }
+                                    nM.SetVisible(false);
+                                    nM.SetState(NavMarkerState.Inactive);
+                                    break;
                                 }
                             }
                         }
@@ -93,29 +93,24 @@ namespace FridosSmartPing
             }
         }
 
-    }*/
+    }
     [HarmonyPatch(typeof(LG_ComputerTerminalCommandInterpreter), nameof(LG_ComputerTerminalCommandInterpreter.ReceiveCommand))]
     public class SmartPingOverridePatch
     {
             static bool Prefix(LG_ComputerTerminalCommandInterpreter __instance, TERM_Command cmd, string inputLine, string param1, string param2)
         {
-            if (inputLine.Contains("SMARTPING"))
+            if (inputLine.StartsWith("SMARTPING"))
             {
                 __instance.m_linesSinceCommand = 0;
                 __instance.m_terminal.IsWaitingForAnyKeyInLinePause = false;
                 __instance.AddOutput(__instance.NewLineStart() + inputLine, false);
-                __instance.AddOutput(__instance.NewLineStart() + "   ______                                      __      _______   __                     ", false);
-                __instance.AddOutput(__instance.NewLineStart() + "  /      \\                                    |  \\    |       \\ |  \\                    ", false);
-                __instance.AddOutput(__instance.NewLineStart() + " |  $$$$$$\\ ______ ____    ______    ______  _| $$_   | $$$$$$$\\ \\$$ _______    ______  ", false);
-                __instance.AddOutput(__instance.NewLineStart() + " | $$___\\$$|      \\    \\  |      \\  /      \\|   $$ \\  | $$__/ $$|  \\|       \\  /      \\ ", false);
-                __instance.AddOutput(__instance.NewLineStart() + "  \\$$    \\ | $$$$$$\\$$$$\\  \\$$$$$$\\|  $$$$$$\\\\$$$$$$  | $$    $$| $$| $$$$$$$\\|  $$$$$$\\", false);
-                __instance.AddOutput(__instance.NewLineStart() + "  _\\$$$$$$\\| $$ | $$ | $$ /      $$| $$   \\$$ | $$ __ | $$$$$$$ | $$| $$  | $$| $$  | $$", false);
-                __instance.AddOutput(__instance.NewLineStart() + " |  \\__| $$| $$ | $$ | $$|  $$$$$$$| $$       | $$|  \\| $$      | $$| $$  | $$| $$__| $$", false);
-                __instance.AddOutput(__instance.NewLineStart() + "  \\$$    $$| $$ | $$ | $$ \\$$    $$| $$        \\$$  $$| $$      | $$| $$  | $$ \\$$    $$", false);
-                __instance.AddOutput(__instance.NewLineStart() + "   \\$$$$$$  \\$$  \\$$  \\$$  \\$$$$$$$ \\$$         \\$$$$  \\$$       \\$$ \\$$   \\$$ _\\$$$$$$$", false);
-                __instance.AddOutput(__instance.NewLineStart() + "                                                                              |  \\__| $$", false);
-                __instance.AddOutput(__instance.NewLineStart() + "          by LolBit & Frido                                                    \\$$    $$", false);
-                __instance.AddOutput(__instance.NewLineStart() + "                                                                                \\$$$$$$ ", false);
+                __instance.AddOutput(__instance.NewLineStart() + "  ____                       _     ____  _             ", false);
+                __instance.AddOutput(__instance.NewLineStart() + " / ___| _ __ ___   __ _ _ __| |_  |  _ \\(_)_ __   __ _ ", false);
+                __instance.AddOutput(__instance.NewLineStart() + " \\___ \\| '_ ` _ \\ / _` | '__| __| | |_) | | '_ \\ / _` |", false);
+                __instance.AddOutput(__instance.NewLineStart() + "  ___) | | | | | | (_| | |  | |_  |  __/| | | | | (_| |", false);
+                __instance.AddOutput(__instance.NewLineStart() + " |____/|_| |_| |_|\\__,_|_|   \\__| |_|   |_|_| |_|\\__, |", false);
+                __instance.AddOutput(__instance.NewLineStart() + "                                                 |___/ ", false);
+                __instance.AddOutput(__instance.NewLineStart() + "          by LolBit & Frido                            ", false);
                 __instance.AddOutput(__instance.NewLineStart(), false);
                 __instance.AddOutput(TerminalLineType.ProgressWait, __instance.NewLineStart() + "Initalizing Fridos Smart Ping™...", 5f, TerminalSoundType.LineTypeDefault, TerminalSoundType.LineTypeDefault);
                 __instance.OnEndOfQueue = new Action(delegate ()
@@ -130,47 +125,51 @@ namespace FridosSmartPing
                             var name = terminalItem.TerminalItemKey;
                             try
                             {
-                                if (current_zone == terminalItem.FloorItemLocation && item.internalSync.GetCurrentState().status != ePickupItemStatus.PickedUp &&
-                                    (name.Contains("PACK") || name.StartsWith("TOOL_REFILL") || name.StartsWith("CARGO_") ||
-                                    name.StartsWith("KEY_") || name.StartsWith("PID_") || name.StartsWith("BULKHEAD_KEY_") ||
-                                    name.StartsWith("DATA_CUBE_") || name.StartsWith("CELL_") || name.StartsWith("GLP_") ||
-                                    name.StartsWith("PD_") || name.StartsWith("OSIP_") || name.StartsWith("HDD_") || name.StartsWith("FOG_TURBINE")))
+                                if (name.ToLower().Contains(param1))
                                 {
-                                    NavMarker m_marker = GuiManager.NavMarkerLayer.PrepareGenericMarker(item.gameObject);
-                                    m_marker.PersistentBetweenRestarts = false;
-                                    if(item.GetCustomData().ammo >= 20)
+                                    if (terminalItem.FloorItemLocation.Contains(current_zone))
                                     {
-                                        m_marker.SetTitle(name + "\n(" + item.GetCustomData().ammo / 20 + " Uses)");
-                                    } else
-                                    {
-                                        m_marker.SetTitle(name);
+                                        NavMarker m_marker = GuiManager.NavMarkerLayer.PrepareGenericMarker(item.gameObject);
+                                        m_marker.PersistentBetweenRestarts = false;
+                                        if (item.GetCustomData().ammo >= 20)
+                                        {
+                                            m_marker.SetTitle(name + "\n(" + item.GetCustomData().ammo / 20 + " Uses)");
+                                        }
+                                        else
+                                        {
+                                            m_marker.SetTitle(name);
+                                        }
+                                        if (name.StartsWith("MEDIPACK"))
+                                        {
+                                            m_marker.SetStyle(eNavMarkerStyle.PlayerPingHealth);
+                                        }
+                                        else if (name.StartsWith("AMMOPACK"))
+                                        {
+                                            m_marker.SetStyle(eNavMarkerStyle.PlayerPingAmmo);
+                                        }
+                                        else if (name.StartsWith("TOOL_REFILL"))
+                                        {
+                                            m_marker.SetStyle(eNavMarkerStyle.PlayerPingToolRefill);
+                                        }
+                                        else if (name.StartsWith("DISINFECT_PACK"))
+                                        {
+                                            m_marker.SetStyle(eNavMarkerStyle.PlayerPingDisinfection);
+                                        }
+                                        else if (name.StartsWith("KEY_"))
+                                        {
+                                            m_marker.SetStyle(eNavMarkerStyle.PlayerPingPickupObjectiveItem);
+                                        }
+                                        else
+                                        {
+                                            m_marker.SetStyle(eNavMarkerStyle.PlayerPingLoot);
+                                        }
+                                        m_marker.SetIconScale(0.4f);
+                                        m_marker.SetAlpha(0.4f);
+                                        m_marker.SetVisible(true);
+                                        m_marker.FadeOutOverTime(20, 10);
+                                        m_marker.m_fadeRoutine = CoroutineManager.StartCoroutine(GuiManager.NavMarkerLayer.FadeMarkerOverTime(m_marker, m_marker.name, UnityEngine.Random.Range(0.1f, 0.5f), 30f, false), null);
+                                        pingedItems += 1;
                                     }
-                                    if (name.StartsWith("MEDIPACK"))
-                                    {
-                                        m_marker.SetStyle(eNavMarkerStyle.PlayerPingHealth);
-                                    }
-                                    else if (name.StartsWith("AMMOPACK"))
-                                    {
-                                        m_marker.SetStyle(eNavMarkerStyle.PlayerPingAmmo);
-                                    }
-                                    else if (name.StartsWith("TOOL_REFILL"))
-                                    {
-                                        m_marker.SetStyle(eNavMarkerStyle.PlayerPingToolRefill);
-                                    }
-                                    else if (name.StartsWith("DISINFECT_PACK"))
-                                    {
-                                        m_marker.SetStyle(eNavMarkerStyle.PlayerPingDisinfection);
-                                    }
-                                    else
-                                    {
-                                        m_marker.SetStyle(eNavMarkerStyle.PlayerPingLoot);
-                                    }
-                                    m_marker.SetIconScale(0.4f);
-                                    m_marker.SetAlpha(0.4f);
-                                    m_marker.SetVisible(true);
-                                    m_marker.FadeOutOverTime(20, 10);
-                                    m_marker.m_fadeRoutine = CoroutineManager.StartCoroutine(GuiManager.NavMarkerLayer.FadeMarkerOverTime(m_marker, m_marker.name, UnityEngine.Random.Range(0.1f, 0.5f), 30f, false), null);
-                                    pingedItems += 1;
                                 }
                             }
                             catch (Exception e)
